91392fbaa723c186624264256bb09c7a796ac477
maxefimov <efimov.top.academy@gmail.com>
2023-12-19 20:34:53 +0300

    Добавьте файлы проекта.

---
+++ NetEventsApp/BankAccount.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetEventsApp
+{
+    class BankAccount
+    {
+        public int Balance { get; private set; }
+
+        AccountHandler? notify;
+
+        // add/remove
+        public event AccountHandler Notify
+        {
+            add
+            {
+                notify += value;
+                Console.WriteLine($"handler {value.Method.Name} add");
+            }
+            remove
+            {
+                notify -= value;
+                Console.WriteLine($"handler {value.Method.Name} remove");
+            }
+        }
+
+        public BankAccount(int sum = 0) => Balance = sum;
+
+        public void AddMoney(int sum)
+        {
+            Balance += sum;
+            notify?.Invoke(this, new AccountEventArgs(sum, $"Balance add {sum} rub."));
+        }
+
+        public void TakeMoney(int sum)
+        {
+            if (Balance >= sum)
+            {
+                Balance -= sum;
+                notify?.Invoke(this, new(-sum, $"Balance del {sum} rub."));
+            }
+            else
+                notify?.Invoke(this, new(-sum, $"Balance less than {sum} rub."));
+
+
+        }
+
+
+        public delegate void AccountHandler(object sender, AccountEventArgs e);
+
+    }
+
+    class AccountEventArgs
+    {
+        public string? Message { get; }
+        public int Sum { get; }
+
+        public AccountEventArgs(int sum, string? message)
+        {
+            Message = message;
+            Sum = sum;
+        }
+    }
+}
---
+++ NetEventsApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using NetEventsApp;
+
+BankAccount account = new BankAccount(1000);
+account.Notify += ConsoleWriter;
+account.Notify += (object sender, AccountEventArgs e) =>
+    {
+        var colorOld = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        if (sender is BankAccount account)
+        {
+            Console.WriteLine($"Summa transaction: {e.Sum}");
+            Console.WriteLine($"Balance: {account.Balance}");
+            Console.WriteLine($"Message {e.Message}");
+        }
+
+        Console.ForegroundColor = colorOld;
+    };
+
+account.AddMoney(500);
+account.TakeMoney(800);
+
+account.Notify -= ConsoleWriter;
+
+account.TakeMoney(800);
+
+void ConsoleWriter(object sender, AccountEventArgs e)
+{
+    if(sender is BankAccount account)
+    {
+        Console.WriteLine($"Summa transaction: {e.Sum}");
+        Console.WriteLine($"Balance: {account.Balance}");
+        Console.WriteLine($"Message {e.Message}");
+    }
+}
